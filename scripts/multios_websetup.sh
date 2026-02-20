2a22dc225a9c7efc4adab1736b54f5ee515adf52
Onyeka Obasiocha <102604104+onyeka30@users.noreply.github.com>
2026-02-20 20:12:48 +0200

    Enhance multios_websetup.sh for better readability
    Refactor web setup script for improved clarity and structure.

--- scripts/multios_websetup.sh
+++ scripts/multios_websetup.sh
@@ -1,115 +1,69 @@
 #!/bin/bash
 
+#######################################################################
+# Script Name: multios_websetup.sh
+# Description: Automated Web Deployment for CentOS and Ubuntu
+# Author: Onyeka
+#######################################################################
+
 # Variable Declaration
-#PACKAGE="httpd wget unzip"
-#SVC="httpd"
 URL='https://www.tooplate.com/zip-templates/2098_health.zip'
 ART_NAME='2098_health'
 TEMPDIR="/tmp/webfiles"
 
+# Check for 'yum' to determine if OS is CentOS/RHEL family
 yum --help &> /dev/null
 
 if [ $? -eq 0 ]
 then
-   # Set Variables for CentOS
-   PACKAGE="httpd wget unzip"
-   SVC="httpd"
+    # --- CentOS Configuration ---
+    PACKAGE="httpd wget unzip"
+    SVC="httpd"
 
-   echo "Running Setup on CentOS"
-   # Installing Dependencies
-   echo "########################################"
-   echo "Installing packages."
-   echo "########################################"
-   sudo yum install $PACKAGE -y > /dev/null
-   echo
+    echo "Running Setup on CentOS/RHEL"
+    echo "########################################"
+    echo "Installing packages..."
+    sudo yum install $PACKAGE -y > /dev/null
 
-   # Start & Enable Service
-   echo "########################################"
-   echo "Start & Enable HTTPD Service"
-   echo "########################################"
-   sudo systemctl start $SVC
-   sudo systemctl enable $SVC
-   echo
-
-   # Creating Temp Directory
-   echo "########################################"
-   echo "Starting Artifact Deployment"
-   echo "########################################"
-   mkdir -p $TEMPDIR
-   cd $TEMPDIR
-   echo
-
-   wget $URL > /dev/null
-   unzip $ART_NAME.zip > /dev/null
-   sudo cp -r $ART_NAME/* /var/www/html/
-   echo
-
-   # Bounce Service
-   echo "########################################"
-   echo "Restarting HTTPD service"
-   echo "########################################"
-   systemctl restart $SVC
-   echo
-
-   # Clean Up
-   echo "########################################"
-   echo "Removing Temporary Files"
-   echo "########################################"
-   rm -rf $TEMPDIR
-   echo
-
-   sudo systemctl status $SVC
-   ls /var/www/html/
+    echo "Starting & Enabling $SVC Service..."
+    sudo systemctl start $SVC
+    sudo systemctl enable $SVC
 
 else
-    # Set Variables for Ubuntu
-   PACKAGE="apache2 wget unzip"
-   SVC="apache2"
+    # --- Ubuntu/Debian Configuration ---
+    PACKAGE="apache2 wget unzip"
+    SVC="apache2"
 
-   echo "Running Setup on CentOS"
-   # Installing Dependencies
-   echo "########################################"
-   echo "Installing packages."
-   echo "########################################"
-   sudo apt update
-   sudo apt install $PACKAGE -y > /dev/null
-   echo
+    echo "Running Setup on Ubuntu/Debian"
+    echo "########################################"
+    echo "Updating cache and installing packages..."
+    sudo apt update > /dev/null
+    sudo apt install $PACKAGE -y > /dev/null
 
-   # Start & Enable Service
-   echo "########################################"
-   echo "Start & Enable HTTPD Service"
-   echo "########################################"
-   sudo systemctl start $SVC
-   sudo systemctl enable $SVC
-   echo
+    echo "Starting & Enabling $SVC Service..."
+    sudo systemctl start $SVC
+    sudo systemctl enable $SVC
+fi
 
-   # Creating Temp Directory
-   echo "########################################"
-   echo "Starting Artifact Deployment"
-   echo "########################################"
-   mkdir -p $TEMPDIR
-   cd $TEMPDIR
-   echo
+# --- Deployment Logic (Common for both OS) ---
+echo "########################################"
+echo "Starting Artifact Deployment"
+echo "########################################"
 
-   wget $URL > /dev/null
-   unzip $ART_NAME.zip > /dev/null
-   sudo cp -r $ART_NAME/* /var/www/html/
-   echo
+mkdir -p $TEMPDIR
+cd $TEMPDIR
 
-   # Bounce Service
-   echo "########################################"
-   echo "Restarting HTTPD service"
-   echo "########################################"
-   systemctl restart $SVC
-   echo
+wget $URL > /dev/null
+unzip $ART_NAME.zip > /dev/null
+sudo cp -r $ART_NAME/* /var/www/html/
 
-   # Clean Up
-   echo "########################################"
-   echo "Removing Temporary Files"
-   echo "########################################"
-   rm -rf $TEMPDIR
-   echo
+echo "Restarting $SVC service..."
+sudo systemctl restart $SVC
 
-   sudo systemctl status $SVC
-   ls /var/www/html/
-fi 
+echo "Removing Temporary Files..."
+rm -rf $TEMPDIR
+
+echo "########################################"
+echo "Deployment Complete!"
+sudo systemctl status $SVC --no-pager
+ls /var/www/html/
